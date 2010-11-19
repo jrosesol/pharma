248dded35700e2a30eca25f75feca2315170c51d
Jose Rose <jrosesol@gmail.com>
2010-11-19 13:34:08 -0500

    + Added file.

--- is/src/main/java/com/pharma/is/server/guice/MyGuiceServletContextListener.java
+++ is/src/main/java/com/pharma/is/server/guice/MyGuiceServletContextListener.java
@@ -3,7 +3,7 @@ package com.pharma.is.server.guice;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
-import com.pharma.is.server.guice.ServerModule;
+import com.pharma.is.server.guice.DispatchServletModule;
 
 
 public class MyGuiceServletContextListener extends GuiceServletContextListener {
